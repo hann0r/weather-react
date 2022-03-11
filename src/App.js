9dc1e47f68340727277f64e1e10d9e1d4b367bee
hann0r <92484605+hann0r@users.noreply.github.com>
2022-03-11 16:37:00 +0000

    Added API intergration

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Cambridge, GB" />
+
+        <footer>
+          Coded by Hannah Harries,{" "}
+          <a
+            href="https://github.com/hann0r/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://stupefied-yonath-695f15.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
