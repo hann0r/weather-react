0dbabc4068379136a54244f0a5e770450cf35152
hann0r <92484605+hann0r@users.noreply.github.com>
2022-04-29 01:32:24 +0100

    Added bold text to App.js

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Cambridge, GB" />
         <p className="Poem">
-          "Whether the weather be fine, <br />
-          or whether the weather be not,
-          <br /> Whether the weather be cold, <br />
-          or whether the weather be hot, <br />
-          We'll weather the weather, <br />
-          whatever the weather, <br />
-          Whether we like it or not.." <br />- Anonymous
+          <strong>
+            "Whether the weather be fine, <br />
+            or whether the weather be not,
+            <br /> Whether the weather be cold, <br />
+            or whether the weather be hot, <br />
+            We'll weather the weather, <br />
+            whatever the weather, <br />
+            Whether we like it or not.." <br />- Anonymous
+          </strong>
         </p>
         <footer>
-          Coded by Hannah Harries,{" "}
+          Coded by <strong>Hannah Harries</strong>,{" "}
           <a
             href="https://github.com/hann0r/weather-react"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Open-sourced on GitHub +
+            Open-sourced on <strong>GitHub</strong> +
           </a>{" "}
           <a
             href="https://stupefied-yonath-695f15.netlify.app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hosted on Netlify
+            Hosted on <strong>Netlify</strong>
           </a>
         </footer>
       </div>
